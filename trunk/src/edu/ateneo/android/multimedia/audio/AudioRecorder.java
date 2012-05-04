77614a9c771f69f4bc254c3756233fe291c18af7
masarapmabuhay <masarapmabuhay@4bf11c46-aabc-b7a4-355d-53fa4f04566a>
2012-05-04 15:35:29 +0000

    May 4, 2012 update: +(work-in-progress)SettingsActivity

--- trunk/src/edu/ateneo/android/multimedia/audio/AudioRecorder.java
+++ trunk/src/edu/ateneo/android/multimedia/audio/AudioRecorder.java
@@ -18,23 +18,47 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import edu.ateneo.android.utils.UsbongUtils;
+
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaRecorder;
 import android.os.Environment;
 
 public class AudioRecorder {
-	MediaRecorder recorder;
+	  String myRecordAudioFileName;
+	  MediaRecorder recorder;
+	  MediaPlayer currMediaPlayer;
 	  String path;
 
 	  /**
 	   * Creates a new audio recording at the given path (relative to root of SD card).
 	   */
-	  public AudioRecorder(String path) 
+	  public AudioRecorder(String path, String myRecordAudioFileName) 
 	  {
 	    this.path = sanitizePath(path);
+	    this.myRecordAudioFileName=myRecordAudioFileName;//this is currUsbongNode
+	  }
+	  
+	  public String getPath() {
+		  return path;
 	  }
 
+	  public boolean isPlaying() {
+		  if (currMediaPlayer!=null) {
+			  return currMediaPlayer.isPlaying();
+		  }
+		  else {
+			  return false;
+		  }
+	  }
+
+	  public void stopPlayback() {
+		  if (currMediaPlayer!=null) {
+			  currMediaPlayer.stop();
+		  }
+	  }
+	  
 	  private String sanitizePath(String path) {
 	    if (!path.startsWith("/")) {
 	      path = "/" + path;
@@ -51,34 +75,38 @@ public class AudioRecorder {
 	  public void start() throws IOException {
 		recorder  = new MediaRecorder();
 	    String state = android.os.Environment.getExternalStorageState();
-
+		
 	    recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 	    recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 	    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
+	    File file = new File(UsbongUtils.BASE_FILE_PATH + UsbongUtils.getTimeStamp()+"/"+myRecordAudioFileName+".3gp");
+       	path =file.getPath();
+
 	    // check if memory card is being used
 	    // UNCOMMENT BELOW TO PERFORM MEMORY CARD SAVING
-	    if(!state.equals(android.os.Environment.MEDIA_MOUNTED))  
+/*
+       	if(!state.equals(android.os.Environment.MEDIA_MOUNTED))  
 	    {
 	    	// instead save on the data directory
-	    	File file = new File(Environment.getDataDirectory().getAbsolutePath()+
-	    			"/data/edu.ateneo.android/test.3gp");
-	       	path =file.getAbsolutePath();
-	        recorder.setOutputFile(path);  
-	    
+//	    	File file = new File(Environment.getDataDirectory().getAbsolutePath()+
+//	    			"/data/edu.ateneo.android/test.3gp");
+	    			
+	        recorder.setOutputFile(path);  	    
 	  	}
 	    else
 	    {
+*/	    	
 	    	// make sure the directory we plan to store the recording in exists	    
 	    	File directory = new File(path).getParentFile();
 	    	if (!directory.exists() && !directory.mkdirs()) 
 	    	{
 	    		throw new IOException("Path to file could not be created.");
 	    	}
+	    	
 		    recorder.setOutputFile(path);
-	    }
+//	    }
 	    // UNCOMMENT ABOVE TO PERFORM MEMORY CARD SAVING
-	        
 	    recorder.prepare();
 	    recorder.start();
 	  }
@@ -90,10 +118,19 @@ public class AudioRecorder {
 	    recorder.stop();
 	    recorder.release();
 	  }
+	  
+	  public boolean hasRecordedData() {
+		    File file = new File(path);
+		    if(file.exists()) {
+		    	return true;
+		    }
+		    return false;
+	  }
 
 	  public void play() throws IOException 
 	  {
 		    MediaPlayer mp = new MediaPlayer();
+		    currMediaPlayer = mp;
 		    File file = new File(path);
 		    try 
 		    {
@@ -119,13 +156,13 @@ public class AudioRecorder {
 		    	public void onCompletion(MediaPlayer mp) {
 		    	mp.release();
 		    	}
-	        });
-			  
+	        });		    		 			  
 	  }
 	  
 	  public void playSaved() throws IOException 
 	  {
 		    MediaPlayer mp = new MediaPlayer();
+		    currMediaPlayer = mp;
 		    File file = new File(path);
 		    try 
 		    {
