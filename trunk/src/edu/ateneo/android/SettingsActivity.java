7fbace36a049de99b850a6bd28c9a89edccf4762
masarapmabuhay <masarapmabuhay@4bf11c46-aabc-b7a4-355d-53fa4f04566a>
2012-06-13 15:17:28 +0000

    Update: Settings page (debug mode and destination url)

--- trunk/src/edu/ateneo/android/SettingsActivity.java
+++ trunk/src/edu/ateneo/android/SettingsActivity.java
@@ -19,20 +19,44 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.EditText;
 import edu.ateneo.android.utils.UsbongUtils;
 
 public class SettingsActivity extends Activity {
+	private CheckBox myDebugModeCheckBox;
+	private CheckBox myDestinationURLCheckBox;
+	private EditText myDestinationURLEditText;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
 	    setContentView(R.layout.settings_screen);	    
-		CheckBox myDebugModeCheckBox = (CheckBox)findViewById(R.id.settings_debug_mode_checkbox);
-	    
+		myDebugModeCheckBox = (CheckBox)findViewById(R.id.settings_debug_mode_checkbox);		
+		myDestinationURLCheckBox = (CheckBox)findViewById(R.id.settings_destination_url_checkbox);		
+		myDestinationURLEditText = (EditText)findViewById(R.id.settings_edittext);		
+				
+		//hide virtual keyboard, instead of auto-focusing on the first edittext
+		myDestinationURLEditText.setInputType(0);	
+		myDestinationURLEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+		    @Override
+		    public void onFocusChange(View v, boolean hasFocus) {
+		        if (hasFocus) {
+					myDestinationURLEditText.setInputType(android.text.InputType.TYPE_CLASS_TEXT);
+		        }
+		    }
+		});
+		
 	    try {	    	
 			InputStreamReader reader = UsbongUtils.getFileFromSDCardAsReader(UsbongUtils.BASE_FILE_PATH + "usbong.config");	
 			BufferedReader br = new BufferedReader(reader);    		
@@ -42,69 +66,105 @@ public class SettingsActivity extends Activity {
 				if (currLineString.equals("IS_IN_DEBUG_MODE=ON")) {
 					myDebugModeCheckBox.setChecked(true);				
 				}
+				if (currLineString.equals("DESTINATION_URL_CHECKBOX=ON")) {
+					myDestinationURLCheckBox.setChecked(true);				
+				}								
+				if (currLineString.contains("DESTINATION_URL=")) {
+					if (myDestinationURLCheckBox.isChecked()) {
+						myDestinationURLEditText.setEnabled(true);
+//						myDestinationURLEditText.setFocusable(true);
+//						myDestinationURLEditText.setFocusableInTouchMode(true);
+//						myDestinationURLEditText.requestFocus();
+					}
+					else {
+						myDestinationURLEditText.setEnabled(false);
+						myDestinationURLEditText.setFocusable(false);
+						myDestinationURLEditText.setFocusableInTouchMode(false);						
+					}
+					myDestinationURLEditText.setText(currLineString.subSequence("DESTINATION_URL=".length(), currLineString.length()));
+				}
 	    	}	        				
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+    	String pressTheMenuButtonToSaveStringValue = (String) getResources().getText(R.string.pressTheMenuButtonToSaveStringValue);
+    	String alertString = (String) getResources().getText(R.string.alertStringValue);
+
+    	new AlertDialog.Builder(SettingsActivity.this).setTitle(alertString)
+		.setMessage(pressTheMenuButtonToSaveStringValue)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {					
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		}).show();	        		        	    	
     }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.save_settings_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case(R.id.save_settings):
+				PrintWriter out = UsbongUtils.getFileFromSDCardAsWriter(UsbongUtils.BASE_FILE_PATH + "usbong.config");
+				if (myDebugModeCheckBox.isChecked()) {
+		    		out.println("IS_IN_DEBUG_MODE=ON");
+				}
+				else {
+		    		out.println("IS_IN_DEBUG_MODE=OFF");					
+				}
+
+				if (myDestinationURLCheckBox.isChecked()) {
+					out.println("DESTINATION_URL_CHECKBOX=ON");
+//		    		out.println("DESTINATION_URL="+myDestinationURLEditText.getText().toString());
+				}				
+				else {
+					out.println("DESTINATION_URL_CHECKBOX=OFF");					
+//		    		out.println("DESTINATION_URL=");
+				}
+				out.println("DESTINATION_URL="+myDestinationURLEditText.getText().toString());
+			    out.close();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
     
 	public void onCheckBoxSettingsDebugModeClicked(View v){
     	if (((CheckBox) v).isChecked()){
     		System.out.println("ON!");
-			PrintWriter out = UsbongUtils.getFileFromSDCardAsWriter(UsbongUtils.BASE_FILE_PATH + "usbong.config");    				
-    		out.println("IS_IN_DEBUG_MODE=ON");
-		    out.close();
-/*
-    		try {
-				InputStreamReader reader = UsbongUtils.getFileFromSDCardAsReader(UsbongUtils.BASE_FILE_PATH + "usbong.config");
-				PrintWriter out = UsbongUtils.getFileFromSDCardAsWriter(UsbongUtils.BASE_FILE_PATH + "usbong.config");    				
-
-				BufferedReader br = new BufferedReader(reader);    		
-	        	String currLineString;        	
-	        	while((currLineString=br.readLine())!=null)
-	        	{ 	
-	        		System.out.println(">>>>>>>>>>>>currLineString"+currLineString);
-	    			if (currLineString.contains("DEBUG_MODE")) {
-	    				currLineString = "IS_IN_DEBUG_MODE=ON";				
-	    			}
-    				out.println(currLineString);
-    				System.out.println(">>>>>>>>>>> ON!"+out.toString());
-	        	}	        
-	        	out.println("testing");
-			    out.close();
-			}
-	    	catch(Exception e) {
-	    		e.printStackTrace();
-	    	}
-*/	    	
+//    		myDebugModeCheckBox.setChecked(true);
     	}
     	else {
     		System.out.println("OFF!");
-			PrintWriter out = UsbongUtils.getFileFromSDCardAsWriter(UsbongUtils.BASE_FILE_PATH + "usbong.config");    				
-    		out.println("IS_IN_DEBUG_MODE=OFF");
-		    out.close();
-/*
-    		try {
-				PrintWriter out = UsbongUtils.getFileFromSDCardAsWriter(UsbongUtils.BASE_FILE_PATH + "usbong.config");    				
-				InputStreamReader reader = UsbongUtils.getFileFromSDCardAsReader(UsbongUtils.BASE_FILE_PATH + "usbong.config");
-
-				BufferedReader br = new BufferedReader(reader);    		
-	        	String currLineString;        	
-	        	while((currLineString=br.readLine())!=null)
-	        	{ 	
-	    			if (currLineString.contains("DEBUG_MODE")) {
-	    				currLineString = "IS_IN_DEBUG_MODE=OFF";				
-	    			}
-    				out.println(currLineString);
-    				System.out.println(">>>>>>>>>>> OFF!"+out.toString());
-	        	}	        				
-			    out.close();
-			}
-	    	catch(Exception e) {
-	    		e.printStackTrace();
-	    	}
-*/	    	
+//    		myDebugModeCheckBox.setChecked(false);
     	}    	
+	}
+	
+	public void onCheckBoxSettingsDestinationURLClicked(View v) {
+    	if (((CheckBox) v).isChecked()){
+			myDestinationURLEditText.setEnabled(true);
+			myDestinationURLEditText.setFocusable(true);
+//			myDestinationURLEditText.setSelected(true);
+			myDestinationURLEditText.setFocusableInTouchMode(true);
+			myDestinationURLEditText.requestFocus();
+			myDestinationURLEditText.setInputType(android.text.InputType.TYPE_CLASS_TEXT);
+    	}
+    	else {
+			myDestinationURLEditText.setEnabled(false);
+			myDestinationURLEditText.setFocusable(false);
+//			myDestinationURLEditText.setSelected(false);
+			myDestinationURLEditText.setFocusableInTouchMode(false);
+			myDestinationURLEditText.setInputType(0);
+    	}    			
 	}
 }
