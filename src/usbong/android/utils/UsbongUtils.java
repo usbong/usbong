a71960f6599ae5272993e24ad2d42cd710b58e4b
Michael Syson <masarapmabuhay@gmail.com>
2016-02-12 09:04:49 +0800

    +added: import java.util.Locale; in UsbongUtils.java, to remove error after adding: dateTimeStamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.US).format(new Date());

--- src/usbong/android/utils/UsbongUtils.java
+++ src/usbong/android/utils/UsbongUtils.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
